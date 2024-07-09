f8594b7e457f8b77f7a45912f6904053eb4a9cc4
Skijearz <doerrie.dominic@web.de>
2024-07-09 02:11:12 +0200

    feat: add SatanicZone message with its features like buffs and zone name

--- src/models/messages/satanic_zone.py
+++ src/models/messages/satanic_zone.py
@@ -1,14 +1,17 @@
 from src.models.messages.base import BaseMessage
 from src.consts.satanic_buffs import satanic_buffs
-
-
+from src.consts.satanic_zone_names import satanic_zone_names
+from src.utils import assets
+from src.consts import assets as assets_const
 class SzBuff:
     buff_name: str
-    buff_description: str
+    buff_description: str | None
     buff_icon: str
     def __init__(self,sz_buff:int):
         self.buff_name = list(satanic_buffs)[sz_buff-1]
         self.buff_description = satanic_buffs.get(self.buff_name)
+        buff_icon = assets_const.get_buff_icon(f"IcBuff_{sz_buff}")
+        self.buff_icon = assets.icon(buff_icon)
 
 
 class SzInfo:
@@ -19,7 +22,15 @@ class SzInfo:
         for buff in sz_buffs.split("|"):
             b = SzBuff(int(buff))
             self.buffs.append(b)
-        self.satanic_zone = sz_zone
+        temp = sz_zone.split("_")
+        sz_act : int = int(temp[1])
+        sz_zone_name_id: int = int(temp[2])
+        all_act_zone_names_of_id : list | None = satanic_zone_names.get(sz_act)
+        if all_act_zone_names_of_id:
+            sz_zone_name = all_act_zone_names_of_id[sz_zone_name_id-1]
+            self.satanic_zone = f"Act {sz_act} : {sz_zone_name}"
+        else:
+            self.satanic_zone = ""
    
 
 
